c7b284b2292d08b6d8571f7a1d2b5155eef03aa2
Wilquer <wilquer.fig@gmail.com>
2023-02-14 18:41:03 -0300

    Personalizacao

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,19 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descrição curso java");
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descrição curso js");
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição Curso JavaScript");
+
         curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("mentoria de java");
-        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
         /*System.out.println(curso1);
